17c31469f1980c670e49387c731525372d84d83f
fadilnatakusumah <fadil.ntksmh@gmail.com>
2021-03-19 19:03:42 +0700

    finishing detail page

--- src/graphql/queries.tsx
+++ src/graphql/queries.tsx
@@ -1,10 +1,45 @@
 import gql from "graphql-tag";
 
 export const QUERY_GET_LIST_POKEMONS = gql`
-  {
-    pokemons{
+  query($limit: Int = 10, $offset: Int = 0){
+    pokemons(limit: $limit, offset: $offset){
+      next
+      nextOffset
+      params
       results{
         name url image id
+      }
+    }
+  }
+`
+
+export const QUERY_GET_POKEMON = gql`
+  query ($name: String!){
+    pokemon(name: $name){
+      id
+      name
+      message
+      height
+      weight
+      moves{
+        move{
+          url
+          name
+        }
+      }
+      abilities{
+        ability{
+          name
+          url
+        }
+      }
+      stats{
+        base_stat
+        effort
+        stat{
+          url
+          name
+        }
       }
     }
   }
--- src/hooks/dataContext.ts
+++
@@ -1,8 +0,0 @@
-import { useContext } from "react";
-import { PokeContext } from "../contexts/ApolloContext";
-
-export const useDataContext = () => {
-  const { pokemons, dispatch } = useContext(PokeContext);
-
-  return { pokemons, dispatch }
-}
